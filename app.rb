000a7223b80ced7d51ee10e3c37c94466be2107e
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-21 18:47:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,7 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
-    "#{params[:phrase]}"
-    "#{params[:phrase]}"
-    "#{params[:phrase]}"
+    number.to_i.times {"#{params[:phrase]}"}
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
